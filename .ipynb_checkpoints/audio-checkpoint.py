28b8fac78c829649c68f38e8a1803b230b9b0277
Evan Wappel <ewappel@gmail.com>
2020-10-20 11:21:52 -0700

    sync

--- .ipynb_checkpoints/audio-checkpoint.py
+++ .ipynb_checkpoints/audio-checkpoint.py
@@ -1,3 +1,8 @@
+catch a star
 youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=PyBDXluVBFk
-    
+
+no sign of testerday
 youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=4BqkIwJzjtI
+
+ragga bomb
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=8eJDTcDUQxQ
