8d68eda0b7bd6915c3d3945561a655b42229ad89
Evan Wappel <ewappel@gmail.com>
2020-10-20 11:11:55 -0700

    sync

--- .ipynb_checkpoints/audio-checkpoint.py
+++ .ipynb_checkpoints/audio-checkpoint.py
@@ -1,1 +1,3 @@
 youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=PyBDXluVBFk
+    
+youtube-dl --extract-audio --audio-format mp3 https://www.youtube.com/watch?v=4BqkIwJzjtI
